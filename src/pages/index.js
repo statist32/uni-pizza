be6643b965bef6def0b53e9f021b837e1a793ee2
lgansel <lgansel@mail.upb.de>
2020-01-13 22:19:26 +0100

    added new components

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,25 +2,30 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 import Hero from "../components/hero"
 import Button from "../components/Button"
+import WelcomeText from "../components/WelcomeText"
+import Food from "../components/Food"
 
+const styles = {
+  "button-wrapper": {
+    display: "flex",
+    justifyContent: "space-evenly",
+    transform: "translateY(-50%)",
+    marginBottom: "15vh",
+  },
+}
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <Hero />
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "space-evenly",
-        transform: "translateY(-50%)",
-      }}
-    >
+    <div style={styles["button-wrapper"]}>
       <Button preText={"Hier klicken zum"} text={"Bestellen"} />
       <Button preText={"Hier klicken zum"} text={"Anrufen"} />
     </div>
+    <WelcomeText />
+    <Food />
   </Layout>
 )
 
