a5ad37f05a94fe5ef7b68d9b4355936e782b7144
lgansel <lgansel@mail.upb.de>
2020-01-17 12:54:01 +0100

    removed unused style

--- src/components/WelcomeText.js
+++ src/components/WelcomeText.js
@@ -1,11 +1,6 @@
 import React from "react"
 import "./WelcomeText.css"
 
-const styles = {
-  h2: { textAlign: "center", fontSize: "2.5rem" },
-  p: { textAlign: "center", fontSize: "1.2rem" },
-}
-
 export default function WelcomeText() {
   return (
     <article className="welcome-text-wrapper">
