f43f53baab23c0e4fab6dcd902971857baef9b0e
lgansel <lgansel@mail.upb.de>
2020-01-17 18:13:24 +0100

    added linting suggestions

--- src/components/Hamburger.js
+++ src/components/Hamburger.js
@@ -2,7 +2,13 @@ import React from "react"
 import "./Hamburger.css"
 export default function Hamburger({ extended, setExtended }) {
   return (
-    <div className="hamburger" onClick={() => setExtended(!extended)}>
+    <div
+      className="hamburger"
+      role="button"
+      tabIndex={0}
+      onClick={() => setExtended(!extended)}
+      onKeyDown={() => setExtended(!extended)}
+    >
       <div className="row">
         <div className={`hamburg ${extended ? "checked" : ""}`}>
           <span className="line"></span>
