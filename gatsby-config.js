87cdd71f9dc041df5327e1529a870d80439f4344
lgansel <lgansel@mail.upb.de>
2020-01-17 17:23:39 +0100

    changed some meta data

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Uni Pizza`,
+    description: `Willkommen auf der Webseite der Uni Pizza. Hier finden Sie alle Informationen zu uns. Wir wünschen Ihnen einen guten Appetit und bis bald!`,
+    author: `@gstatist32`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -18,13 +18,10 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Uni Pizza`,
+        short_name: `Uni Pizza`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/Pizza_icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
